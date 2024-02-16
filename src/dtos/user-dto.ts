bb25b231aebf5a9bdc4860246c17e938f4f9ad93
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-02-16 21:35:33 +0800

    Changed user data

--- src/dtos/user-dto.ts
+++ src/dtos/user-dto.ts
@@ -3,13 +3,11 @@ export class UserDto {
   username: string;
   avatar: string;
   createdAt: string;
-  isAuth: boolean
 
   constructor(model: any) {
     this.email = model.email;
     this.username = model.username;
     this.avatar = model.avatar;
     this.createdAt = model.created_at;
-    this.isAuth = model.is_auth;
   }
 }
